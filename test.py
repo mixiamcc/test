be2dc3897a57f11373b040d3f2df5b98e1f7e960
mcc <50347836+mixiamcc@users.noreply.github.com>
2019-05-12 17:26:07 +0800

    dakajishu
    打卡计数

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print('git')
+import os
+
+n = 0
+line = list()
+with open('log_files/201811113016.log',encoding='utf-8') as dir_list:
+  for line in dir_list:
+    line = dir_list.readline()
+    number = line[14:26]
+    if  number == '201811113016':
+      n=n+1
+print(n)   
+dir_list.close()
+
+
+
+
+
+
+
+
